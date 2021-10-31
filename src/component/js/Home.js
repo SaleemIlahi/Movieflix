59510c5435845d85c0fede37c80ab4ad21dbe1de
Mohamed Saleem <saleemilahi21@gmail.com>
2021-10-31 14:28:37 +0530

    Updated Home Page

---
+++ src/component/js/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../scss/Home.scss'
+import logo from '../../logo.svg'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <header>
+                <nav>
+                    <img src={logo} alt="logo" />
+                    <button>Sign in</button>
+                </nav>
+            </header>
+            <div className="text-section">
+                <h1>Unlimited movies, TV shows and more.</h1>
+                <h2>Watch anywhere. Cancel anytime.</h2>
+                <p>Ready to watch? Enter your email to create or restart your membership.</p>
+                <form className="input-section">
+                    <input type="email" placeholder="Email Address" required />
+                    <button>
+                        <Link to="/Page">Get Started <i className="fas fa-greater-than"></i></Link>
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Home
