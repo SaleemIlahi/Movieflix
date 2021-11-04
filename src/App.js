224183493398fb6de384a97114f697d02cdbaa45
Mohamed Saleem <saleemilahi21@gmail.com>
2021-11-04 12:45:45 +0530

    Added HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import Home from './component/js/Home'
 import Page from './component/js/Page'
 import Youtube from './component/js/Youtube'
 const App = () => {
     return (
         <main>
-            <BrowserRouter>
+            <HashRouter>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/Page" component={Page} />
                 <Route exact path="/Youtube"component={Youtube} />
-            </BrowserRouter>
+            </HashRouter>
         </main>
     )
 }
